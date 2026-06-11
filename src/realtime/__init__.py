dd55f6cb4e702186c8a9235bd246b747d2acfafe
phu le <lekimphu2005@gmail.com>
2026-06-11 21:21:10 +0700

    Remove unused files: old realtime implementation, old dashboard, old docs

--- src/realtime/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Realtime Prediction Module
-Hệ thống dự đoán giá crypto theo thời gian thực
-"""
-
-from .data_collector import RealtimeDataCollector
-from .predictor import RealtimePredictor
-
-__all__ = ['RealtimeDataCollector', 'RealtimePredictor']
